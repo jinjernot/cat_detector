1b1dd94a2dd9d1d6ecb051b9ca9f75a7c7cb1725
norindes <norindes@gmail.com>
2024-07-25 19:32:07 -0600

    cube test

--- main.py
+++ main.py
@@ -6,12 +6,10 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
-6
-detect_cats()
 
 @app.route('/video_feed')
 def video_feed():
     return Response(detect_cats(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', debug=True)
