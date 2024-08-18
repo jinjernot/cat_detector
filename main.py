4d45fabe7de09a0e31766419f0cbbc56400feaa4
norindes <norindes@gmail.com>
2024-08-18 12:34:11 -0600

    structure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, Response, request
-from app.cat import detect_cats
-from config import RTSP_URLS
+from app.core.stream_handler import process_video_stream
+from app.settings.streams import RTSP_URLS
 
 app = Flask(__name__)
 
@@ -14,7 +14,7 @@ def index():
 def video_feed():
     global current_stream_index
     rtsp_url = RTSP_URLS[current_stream_index]
-    return Response(detect_cats(rtsp_url), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(process_video_stream(rtsp_url), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @app.route('/switch_stream', methods=['POST'])
 def switch_stream():
